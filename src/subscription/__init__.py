039f25a30f5426e91628ff5304f6d594def221a5
rand0m <rand0m42195@gmail.com>
2025-07-12 23:07:37 +0800

    fix: remove non-existent SubscriptionAPI import from __init__.py

--- src/subscription/__init__.py
+++ src/subscription/__init__.py
@@ -3,7 +3,6 @@ Subscription functionality for Github Trending History.
 """
 
 from .manager import SubscriptionManager
-from .api import SubscriptionAPI
 from .processor import process_subscription_issues
 
-__all__ = ['SubscriptionManager', 'SubscriptionAPI', 'process_subscription_issues'] 
+__all__ = ['SubscriptionManager', 'process_subscription_issues'] 
